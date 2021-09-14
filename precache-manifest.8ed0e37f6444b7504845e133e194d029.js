34f5ce3797de6a89aceb17388d516cdbcafe38d2
tushar-kant <tusharkantanayak713@gmail.com>
2021-09-14 20:12:41 +0530

    Updates

---
+++ precache-manifest.8ed0e37f6444b7504845e133e194d029.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0cbc2d110cfaec434eb14ae7402f66d",
+    "url": "/eth-ipfs-storage/index.html"
+  },
+  {
+    "revision": "862440a64d5aeebf8fff",
+    "url": "/eth-ipfs-storage/static/css/2.9937f3fc.chunk.css"
+  },
+  {
+    "revision": "07c007338428ac2e43fc",
+    "url": "/eth-ipfs-storage/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "862440a64d5aeebf8fff",
+    "url": "/eth-ipfs-storage/static/js/2.b626b980.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "/eth-ipfs-storage/static/js/2.b626b980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07c007338428ac2e43fc",
+    "url": "/eth-ipfs-storage/static/js/main.d1162075.chunk.js"
+  },
+  {
+    "revision": "48ea50f4b91e1c6f6e15",
+    "url": "/eth-ipfs-storage/static/js/runtime-main.4b88e40f.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "/eth-ipfs-storage/static/media/box.df8b84d5.png"
+  }
+]);
